c8196f8f9957ef85599374bf9714f8ccd7ce1cd6
Thapelo Malete <thapelomalet066@gmail.com>
2024-02-19 01:54:09 +0200

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const navButton = document.querySelector('.nav-btn');
+const nav = document.querySelector('.nav');
+
+navButton.addEventListener("click",()=> {
+    nav.classList.toggle('vissible')
+})
